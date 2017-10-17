88794973586fcaaddb1efee0998c682595b9e826
U_M0V1M <akokoreva@alfabank.ru>
2017-10-17 17:31:32 +0300

    домашка

--- src/homework-arrays-and-objects.js
+++ src/homework-arrays-and-objects.js
@@ -1,50 +1,20 @@
+
 export default function getGroupedOperations(operations) {
-  const arr = operations
-    .slice()
-    .map(convertOperationsToItem)
-    .sort(compareObjects);
-  let i = 1;
-  while (arr[i] !== undefined) {
-    if (isEqualDates(arr[i], arr[i - 1])) {
-      addToDay(arr[i - 1], arr[i]);
-      arr.splice(i, 1);
-    } else i++;
-  }
-  return arr;
-}
-
-
-function isEqualDates(firstItem, secondItem) {
-  if (Object.keys(firstItem)[0] === Object.keys(secondItem)[0]) {
-    return true;
-  } else return false;
-}
-
-function convertOperationsToItem(object) {
-  let key = object.date;
-  let item = {};
-  item[key] = {
-    operations: [],
-    links: []
-  };
-  item[key].operations.push(object);
-  object.links.forEach(link => {
-    item[key].links.push(link);
-  });
-  return item;
-}
-
-
-function addToDay(previous, current) {
-  currentItem = current[Object.keys(current)[0]];
-  previousItem = previous[Object.keys(previous)[0]];
-  currentItem.operations.forEach(operation => {
-    previousItem.operations.push(operation);
-  });
-  currentItem.links.forEach(link => {
-    if (!linkIsExist(previousItem.links, link))
-      previousItem.links.push(link)
-  });
+return operations
+  .reduce(function(obj, operation) {
+    if (obj[operation.date] === undefined) {
+      obj[operation.date] = {
+        operations: [],
+        links: []
+      };
+    }
+    obj[operation.date].operations.push(operation);
+    operation.links.forEach(link => {
+      if (!linkIsExist(obj[operation.date].links, link))
+        obj[operation.date].links.push(link)
+    });
+    return obj;
+  }, {});
 }
 
 function linkIsExist(previousLinksArray, linkForCompare) {
@@ -54,12 +24,4 @@ function linkIsExist(previousLinksArray, linkForCompare) {
   const currentHref = linkForCompare.href;
   if (hrefArray.indexOf(currentHref) === -1) return false;
   else return true;
-}
-
-function compareObjects(firstObj, secondObj) {
-  return compareDates(Object.keys(firstObj)[0], Object.keys(secondObj)[0]);
-}
-
-function compareDates(firstDate, secondDate) {
-  return new Date(secondDate) - new Date(firstDate);
 }
